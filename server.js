3a6d16ffddf666b866975d98889dbba5d98b2532
SAIROHITHARETI <areetirohith@gmail.com>
2016-09-21 03:25:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res) {
-   res.send('article two requested will  be displayed her');
+   res.send('article one requested will  be displayed her');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
