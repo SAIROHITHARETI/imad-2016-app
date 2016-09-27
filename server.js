2fe80455e9e698b4f6beffd153fd544ce9ed6c83
SAIROHITHARETI <areetirohith@gmail.com>
2016-09-27 19:30:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,6 +65,11 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req, res) {
   res.send(createtemplete(articleone));
 });
+counter=0;
+app.get('/counter',function(req, res) {
+    counter=counter+1;
+  res.send('counter.tostring()');
+});
 
 app.get('/article-two',function(req, res) {
   res.sendFile(path.join(__dirname, 'ui','article-two.html'));
