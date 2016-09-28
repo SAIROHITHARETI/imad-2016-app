6b0da5b89d77fe146864b6620e51a1c50fd2fa75
SAIROHITHARETI <areetirohith@gmail.com>
2016-09-28 23:53:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,19 @@
 
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange =function(){
+        if(request.readyState===XMLHttprequest.DONE)
+        { 
+        if(request.status===200) 
+         {
+        var counter=request.responseText;
+         var span=document.getElementById('count');
+         span.innerHTML=counter.toString();
+        }
+      }    
+    };
+ request.open('GET','sairohithareti.imad.hasura-app.io',true);
+ request.send(null);
     
-    
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-}
+};  
