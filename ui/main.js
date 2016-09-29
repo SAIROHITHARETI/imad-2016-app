d844fcab91558b600c5c4dd01e29ee034f05eea1
SAIROHITHARETI <areetirohith@gmail.com>
2016-09-30 01:51:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,18 @@ button.onclick=function(){
  request.send(null);
     
 };  
+
+var nameinput=document.getElementById('NAME');
+var name=nameinput.value
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){ 
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list='<li>'+names[i]+'</li>';
+        
+    }
+    var ul=document.getElementById('fuck');
+    ul.innerHTML=list;
+    
+};
