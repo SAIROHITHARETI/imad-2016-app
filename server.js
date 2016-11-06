e2a86e722722c3848706ce7df7804aa961a98761
SAIROHITHARETI <areetirohith@gmail.com>
2016-11-07 03:33:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool=require('pg').Pool;
-
-var config {
-    user: 'sairohithareti',
-    database: 'sairohithareti',
-    host:'db.imad.hasura.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
 
 
 var app = express();
@@ -72,19 +63,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool=new Pool(config)
-app.get('/test_db'),function(req,res){
-    pool.query('SELECT * FROM test',function(err,result){
-        if(err){
-        res.status(500).send(err.toString());
-        }
-        else{ 
-            res.send(JSON.stringify(result));
-        }
-        
-    });
-    
-});
+
 
 app.get('/article-one',function(req, res) {
   res.send(createtemplete(articleone));
