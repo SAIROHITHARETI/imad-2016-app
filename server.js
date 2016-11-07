25d26f6edbc67c55fc3ffba9dab363ced5a7226c
SAIROHITHARETI <areetirohith@gmail.com>
2016-11-07 22:15:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 
-var config {
+var config ={
     user: 'sairohithareti',
     database:'sairohithareti',
     host: 'db.imad.hasura.io',
