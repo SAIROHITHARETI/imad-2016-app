48d65930346302531e529df7ff6e00d30f5f259d
SAIROHITHARETI <areetirohith@gmail.com>
2016-11-07 22:23:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,9 +73,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool=new Pool(config);
+var pool = new Pool(config);
 
-app.get('/test_db',function(req,res){
+app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         if(err){
         res.status(500).send(err.toString());
