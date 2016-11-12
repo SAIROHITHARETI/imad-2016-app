6bbd5f8064a057618fb9d5de03fec7a8fe9bfccf
SAIROHITHARETI <areetirohith@gmail.com>
2016-11-12 22:15:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 
 var config ={
     user: 'sairohithareti',
@@ -16,6 +17,7 @@ var config ={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articleone={
     title:'article-one| sairohith',
@@ -83,6 +85,22 @@ app.get('/hash/:input', function(req,res){
     var hashedString= hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
     
+});
+app.post('/create-user',function(req,res){
+    var name=req.body.name;
+    var password=req.body.password;
+   
+   var salt=crypto.randomBytes(128).toString('hex');
+   var dbString=hash(password,salt); 
+   pool.query('INSERT INTO "user"(name,password) VALUES($1,$2)',[name,password],function(req,res){
+        if(err){
+        res.status(500).send(err.toString());
+        }
+        else{ 
+            res.send('user successfully created:'+name);
+        }
+       
+   });
 });
 
 
