3dcbe38c0bf0e4aea149bd679dc676e0be6295f3
SAIROHITHARETI <areetirohith@gmail.com>
2016-11-13 06:38:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,3 +33,31 @@ submit.onclick=function(){
     ul.innerHTML=list;
     
 };
+var button=document.getElementById('submit_btn1');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange =function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        { 
+        if(request.status===200) {
+           console.log('userlogged in!');
+           alert('logged in successfully');
+            }
+            else if(request.status===403){
+                alert('username/password is incorrect');
+        }
+        else if(request.status===500){
+            alert('something went wrong!!');
+        }
+      }    
+    };
+    var username=getElementById('name1').value;
+    var password=getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    var nameInput=nameInput.value;
+     request.open('POST','http://sairohithareti.imad.hasura-app.io/login',true);
+     request.setRequestHeader('Content-Type','application/json');
+     request.send(JSON.stringify({name:username ,password:password}));
+    
+};  
